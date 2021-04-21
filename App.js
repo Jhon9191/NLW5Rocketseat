97e95850a51250e2261651fb7dd5f79bba35c94b
Jhon9191 <joaoantonio15161516@gmail.com>
2021-04-20 23:56:09 -0300

    Create react-native-app

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello NLW5</Text>
+    </View>
+  );
+};
+
+
+export default App;
