b999009b0881df3753e86f7a3e4838a9c3e300ee
Jhon9191 <joaoantonio15161516@gmail.com>
2021-04-26 16:08:56 -0300

    Configuring fake api

--- src/screens/PlantSelected/index.tsx
+++ src/screens/PlantSelected/index.tsx
@@ -32,13 +32,13 @@ const PlantSelected = () => {
                         showsHorizontalScrollIndicator={false}
                         contentContainerStyle={styles.eviromentList}
                         data={[
-                        {id: "1", title: "1"},
-                        {id: "2", title: "2"},
-                        {id: "3", title: "3"},
-                        {id: "4", title: "4"},
-                        {id: "5", title: "5"}
+                        {id: 1, title: "1"},
+                        {id: 2, title: "2"},
+                        {id: 3, title: "3"},
+                        {id: 4, title: "4"},
+                        {id: 5, title: "5"}
                         ]}
-                        keyExtractor={(item) => item.toString()}
+                        keyExtractor={(item) => item.id.toString()}
                         renderItem={({item})=>(
                             <EnviromentButton title={`${item.title}`} active/>
                         )}/>
