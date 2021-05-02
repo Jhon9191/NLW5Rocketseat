ef34f9f2678c9bffc429e7e5d34d9d332ce9b1e7
Jhon9191 <joaoantonio15161516@gmail.com>
2021-05-01 23:56:58 -0300

    Adding time picker for alert

--- src/screens/Selected/index.tsx
+++ src/screens/Selected/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     SafeAreaView,
     Text,
     View,
     Image,
+    Platform,
+    Alert
 } from 'react-native';
 import { SvgFromUri } from 'react-native-svg';
 import styles from './styles';
 import { useRoute } from '@react-navigation/native'
+import Datetimepicker, { Event } from '@react-native-community/datetimepicker';
 import waterDrop from '../../assets/waterdrop.png';
 import Button from '../../components/Button';
+import { isBefore } from 'date-fns';
 
 interface PlantsProps {
     item: {
@@ -30,6 +34,23 @@ const Selected = () => {
 
     const route = useRoute();
     const { item } = route.params as PlantsProps;
+    const [ selectedTime,  setSelectedTime ] = useState(new Date);
+    const [ showDatePicker, setShowDatePicker ] = useState(true);
+
+    const handleSetTime = (Event: Event, dateTime : Date | undefined) => {
+        if(Platform.OS === "android"){
+            setShowDatePicker(oldValue => !oldValue);
+        }
+
+        if(dateTime && isBefore(dateTime, new Date())){
+            setSelectedTime(new Date());
+            return Alert.alert("Escolha uma hora do futuro!")
+        }
+
+        if(dateTime )
+            setSelectedTime(dateTime)
+
+    }
 
     return (
         <SafeAreaView style={styles.container}>
@@ -54,6 +75,15 @@ const Selected = () => {
                 </View>
 
                 <Text style={styles.alertLabel}>Escolha o melhor horiario</Text>
+
+                {showDatePicker && (
+                <Datetimepicker
+                    value={selectedTime}
+                    mode="time"
+                    display="spinner"
+                    onChange={handleSetTime}
+                />
+                )}
 
                 <Button title="Cadastrar Planta" onPress={() => { }} />
 
